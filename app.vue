bd835328866662259bd1d2f873ae64134c3549e2
Yannis Storrer <yannis.storrer@web.de>
2024-03-27 14:56:16 +0100

    Added texteditor component

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<body>
+		<div class="main">
+			<editor></editor>
+ 		</div>
+	</body> 
 </template>
+
+<style>
+.main {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  overflow: hidden;
+  background: lime;
+  padding: 0px;
+  margin: 0px;
+}
+
+html, body {
+  padding: 0px;
+  margin: 0px;
+}
+</style>
---
+++ components/editor.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="editor">
+		<textarea id="editor-textarea">; Enter your assembly code here.</textarea>
+	</div>
+</template>
+<style scoped>
+textarea {
+  	border: 0;
+	resize: none;
+	outline: none;
+	box-sizing: border-box;
+	width: 100%;
+	height: 100%;
+}
+.editor {
+	padding: 0px;
+	margin: 0px;
+	width: 100%;
+	height: 100%;
+}
+</style>
